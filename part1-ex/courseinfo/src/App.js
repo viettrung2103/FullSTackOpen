ecc3bb8394e76d6a6e865f5470815f74a8686044
viettrung2103 <viettrung21@gmail.com>
2022-09-29 00:28:34 +0300

    render all parts into partList and put them as child components for Content component

--- part1-ex/courseinfo/src/App.js
+++ part1-ex/courseinfo/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = (props) => {
+  return(
+  <div>
+    <h1>{props.course}</h1>
+  </div>
+  )
 }
 
-export default App;
+
+const Content = (props) => {
+  const partList = [{
+    name:'Fundamentals of React',
+    exercise:10,
+  },{
+    name:'Using props to pass data',
+    exercise:7,
+  },{
+    name:'State of a component',
+    exercise:14,
+  },];
+  return(
+    <div>
+      <Part partList = {partList}/>
+      
+  </div>
+  )
+}
+
+const Part = (props) => {
+  return props.partList.map((part)=>{
+    return(
+    <div>
+      Part {part.name} has {part.exercise} exercises.
+    </div>
+    )
+  })
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercise {props.ex}</p>
+    </div>
+  )
+}
+
+
+
+//Header takes care of rendering the name of the course
+//Content renders the parts and their number of exercise
+//Total renders the total number of exercise
+const App = () => {
+  const course = 'Half Stack application development '
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content />
+      <Total ex = {exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
